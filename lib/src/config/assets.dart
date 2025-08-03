cf8e38a9d525d9fec3d8aec31bced1f8250cb4c3
ahmed ibrahim mousa <ahmed0128382@gmail.com>
2025-08-03 23:34:30 +0300

    set up a little nice app icon and native splash

---
+++ lib/src/config/assets.dart
@@ -0,0 +1,197 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsFontsCairoBold
+  /// assets/fonts/Cairo-Bold.ttf
+  static const String assetsFontsCairoBold = "assets/fonts/Cairo-Bold.ttf";
+
+  /// Assets for assetsFontsCairoExtraBold
+  /// assets/fonts/Cairo-ExtraBold.ttf
+  static const String assetsFontsCairoExtraBold = "assets/fonts/Cairo-ExtraBold.ttf";
+
+  /// Assets for assetsFontsCairoExtraLight
+  /// assets/fonts/Cairo-ExtraLight.ttf
+  static const String assetsFontsCairoExtraLight = "assets/fonts/Cairo-ExtraLight.ttf";
+
+  /// Assets for assetsFontsCairoLight
+  /// assets/fonts/Cairo-Light.ttf
+  static const String assetsFontsCairoLight = "assets/fonts/Cairo-Light.ttf";
+
+  /// Assets for assetsFontsCairoMedium
+  /// assets/fonts/Cairo-Medium.ttf
+  static const String assetsFontsCairoMedium = "assets/fonts/Cairo-Medium.ttf";
+
+  /// Assets for assetsFontsCairoRegular
+  /// assets/fonts/Cairo-Regular.ttf
+  static const String assetsFontsCairoRegular = "assets/fonts/Cairo-Regular.ttf";
+
+  /// Assets for assetsFontsCairoSemiBold
+  /// assets/fonts/Cairo-SemiBold.ttf
+  static const String assetsFontsCairoSemiBold = "assets/fonts/Cairo-SemiBold.ttf";
+
+  /// Assets for assetsFontsInter18ptBold
+  /// assets/fonts/Inter_18pt-Bold.ttf
+  static const String assetsFontsInter18ptBold = "assets/fonts/Inter_18pt-Bold.ttf";
+
+  /// Assets for assetsFontsInter18ptExtraBold
+  /// assets/fonts/Inter_18pt-ExtraBold.ttf
+  static const String assetsFontsInter18ptExtraBold = "assets/fonts/Inter_18pt-ExtraBold.ttf";
+
+  /// Assets for assetsFontsInter18ptLight
+  /// assets/fonts/Inter_18pt-Light.ttf
+  static const String assetsFontsInter18ptLight = "assets/fonts/Inter_18pt-Light.ttf";
+
+  /// Assets for assetsFontsInter18ptMedium
+  /// assets/fonts/Inter_18pt-Medium.ttf
+  static const String assetsFontsInter18ptMedium = "assets/fonts/Inter_18pt-Medium.ttf";
+
+  /// Assets for assetsFontsInter18ptRegular
+  /// assets/fonts/Inter_18pt-Regular.ttf
+  static const String assetsFontsInter18ptRegular = "assets/fonts/Inter_18pt-Regular.ttf";
+
+  /// Assets for assetsFontsInter18ptSemiBold
+  /// assets/fonts/Inter_18pt-SemiBold.ttf
+  static const String assetsFontsInter18ptSemiBold = "assets/fonts/Inter_18pt-SemiBold.ttf";
+
+  /// Assets for assetsFontsInter18ptThin
+  /// assets/fonts/Inter_18pt-Thin.ttf
+  static const String assetsFontsInter18ptThin = "assets/fonts/Inter_18pt-Thin.ttf";
+
+  /// Assets for assetsFontsInter24ptBold
+  /// assets/fonts/Inter_24pt-Bold.ttf
+  static const String assetsFontsInter24ptBold = "assets/fonts/Inter_24pt-Bold.ttf";
+
+  /// Assets for assetsFontsInter24ptExtraBold
+  /// assets/fonts/Inter_24pt-ExtraBold.ttf
+  static const String assetsFontsInter24ptExtraBold = "assets/fonts/Inter_24pt-ExtraBold.ttf";
+
+  /// Assets for assetsFontsInter24ptLight
+  /// assets/fonts/Inter_24pt-Light.ttf
+  static const String assetsFontsInter24ptLight = "assets/fonts/Inter_24pt-Light.ttf";
+
+  /// Assets for assetsFontsInter24ptMedium
+  /// assets/fonts/Inter_24pt-Medium.ttf
+  static const String assetsFontsInter24ptMedium = "assets/fonts/Inter_24pt-Medium.ttf";
+
+  /// Assets for assetsFontsInter24ptRegular
+  /// assets/fonts/Inter_24pt-Regular.ttf
+  static const String assetsFontsInter24ptRegular = "assets/fonts/Inter_24pt-Regular.ttf";
+
+  /// Assets for assetsFontsInter24ptSemiBold
+  /// assets/fonts/Inter_24pt-SemiBold.ttf
+  static const String assetsFontsInter24ptSemiBold = "assets/fonts/Inter_24pt-SemiBold.ttf";
+
+  /// Assets for assetsFontsInter24ptThin
+  /// assets/fonts/Inter_24pt-Thin.ttf
+  static const String assetsFontsInter24ptThin = "assets/fonts/Inter_24pt-Thin.ttf";
+
+  /// Assets for assetsFontsInter28ptBold
+  /// assets/fonts/Inter_28pt-Bold.ttf
+  static const String assetsFontsInter28ptBold = "assets/fonts/Inter_28pt-Bold.ttf";
+
+  /// Assets for assetsFontsInter28ptExtraBold
+  /// assets/fonts/Inter_28pt-ExtraBold.ttf
+  static const String assetsFontsInter28ptExtraBold = "assets/fonts/Inter_28pt-ExtraBold.ttf";
+
+  /// Assets for assetsFontsInter28ptLight
+  /// assets/fonts/Inter_28pt-Light.ttf
+  static const String assetsFontsInter28ptLight = "assets/fonts/Inter_28pt-Light.ttf";
+
+  /// Assets for assetsFontsInter28ptMedium
+  /// assets/fonts/Inter_28pt-Medium.ttf
+  static const String assetsFontsInter28ptMedium = "assets/fonts/Inter_28pt-Medium.ttf";
+
+  /// Assets for assetsFontsInter28ptRegular
+  /// assets/fonts/Inter_28pt-Regular.ttf
+  static const String assetsFontsInter28ptRegular = "assets/fonts/Inter_28pt-Regular.ttf";
+
+  /// Assets for assetsFontsInter28ptSemiBold
+  /// assets/fonts/Inter_28pt-SemiBold.ttf
+  static const String assetsFontsInter28ptSemiBold = "assets/fonts/Inter_28pt-SemiBold.ttf";
+
+  /// Assets for assetsFontsInter28ptThin
+  /// assets/fonts/Inter_28pt-Thin.ttf
+  static const String assetsFontsInter28ptThin = "assets/fonts/Inter_28pt-Thin.ttf";
+
+  /// Assets for assetsFontsMontserratBold
+  /// assets/fonts/Montserrat-Bold.ttf
+  static const String assetsFontsMontserratBold = "assets/fonts/Montserrat-Bold.ttf";
+
+  /// Assets for assetsFontsMontserratExtraBold
+  /// assets/fonts/Montserrat-ExtraBold.ttf
+  static const String assetsFontsMontserratExtraBold = "assets/fonts/Montserrat-ExtraBold.ttf";
+
+  /// Assets for assetsFontsMontserratLight
+  /// assets/fonts/Montserrat-Light.ttf
+  static const String assetsFontsMontserratLight = "assets/fonts/Montserrat-Light.ttf";
+
+  /// Assets for assetsFontsMontserratMedium
+  /// assets/fonts/Montserrat-Medium.ttf
+  static const String assetsFontsMontserratMedium = "assets/fonts/Montserrat-Medium.ttf";
+
+  /// Assets for assetsFontsMontserratRegular
+  /// assets/fonts/Montserrat-Regular.ttf
+  static const String assetsFontsMontserratRegular = "assets/fonts/Montserrat-Regular.ttf";
+
+  /// Assets for assetsFontsMontserratSemiBold
+  /// assets/fonts/Montserrat-SemiBold.ttf
+  static const String assetsFontsMontserratSemiBold = "assets/fonts/Montserrat-SemiBold.ttf";
+
+  /// Assets for assetsFontsPoppinsBold
+  /// assets/fonts/Poppins-Bold.ttf
+  static const String assetsFontsPoppinsBold = "assets/fonts/Poppins-Bold.ttf";
+
+  /// Assets for assetsFontsPoppinsExtraBold
+  /// assets/fonts/Poppins-ExtraBold.ttf
+  static const String assetsFontsPoppinsExtraBold = "assets/fonts/Poppins-ExtraBold.ttf";
+
+  /// Assets for assetsFontsPoppinsLight
+  /// assets/fonts/Poppins-Light.ttf
+  static const String assetsFontsPoppinsLight = "assets/fonts/Poppins-Light.ttf";
+
+  /// Assets for assetsFontsPoppinsMedium
+  /// assets/fonts/Poppins-Medium.ttf
+  static const String assetsFontsPoppinsMedium = "assets/fonts/Poppins-Medium.ttf";
+
+  /// Assets for assetsFontsPoppinsRegular
+  /// assets/fonts/Poppins-Regular.ttf
+  static const String assetsFontsPoppinsRegular = "assets/fonts/Poppins-Regular.ttf";
+
+  /// Assets for assetsFontsPoppinsSemiBold
+  /// assets/fonts/Poppins-SemiBold.ttf
+  static const String assetsFontsPoppinsSemiBold = "assets/fonts/Poppins-SemiBold.ttf";
+
+  /// Assets for assetsIconsAppIcon
+  /// assets/icons/app_icon.png
+  static const String assetsIconsAppIcon = "assets/icons/app_icon.png";
+
+  /// Assets for assetsImagesSplashLogo
+  /// assets/images/splash_logo.png
+  static const String assetsImagesSplashLogo = "assets/images/splash_logo.png";
+
+  /// Assets for assetsImagesSplashLogoTwo
+  /// assets/images/splash_logo__two.png
+  static const String assetsImagesSplashLogoTwo = "assets/images/splash_logo__two.png";
+
+  /// Assets for assetsImagesZLogo
+  /// assets/images/z_logo.jpg
+  static const String assetsImagesZLogo = "assets/images/z_logo.jpg";
+
+  /// Assets for assetsImagesZentryAndZ
+  /// assets/images/zentry_and_z.jpg
+  static const String assetsImagesZentryAndZ = "assets/images/zentry_and_z.jpg";
+
+  /// Assets for assetsImagesZentryAndZThree
+  /// assets/images/zentry_and_z_three.jpg
+  static const String assetsImagesZentryAndZThree = "assets/images/zentry_and_z_three.jpg";
+
+  /// Assets for assetsImagesZentryAndZTwo
+  /// assets/images/zentry_and_z_two.jpg
+  static const String assetsImagesZentryAndZTwo = "assets/images/zentry_and_z_two.jpg";
+
+  /// Assets for assetsImagesZentrySplash
+  /// assets/images/zentry_splash.png
+  static const String assetsImagesZentrySplash = "assets/images/zentry_splash.png";
+}
+
